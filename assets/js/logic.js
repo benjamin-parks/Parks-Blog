a73889872efc557234c0f0c0b0b649875707f8a8
Ben Parks <benparks93@gmail.com>
2024-03-27 23:04:19 -0500

    Fixed darkmode toggling off on refresh or page swap

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,11 +2,20 @@ console.log("I am linked");
 const body = document.body;
 const button = document.getElementById("toggle");
 
+let state = localStorage.getItem("class");
+
+if (state === "dark-mode"){
+    body.setAttribute("class", "dark-mode");
+}
+
 button.addEventListener("click", function(){
     if (body.getAttribute("class", "dark-mode")){
         body.removeAttribute("class", "dark-mode");
+        localStorage.removeItem("class")
     }
     else {
         body.setAttribute("class", "dark-mode");
+        localStorage.setItem("class", "dark-mode");
+        console.log("dark mode toggled")
     }
 })
